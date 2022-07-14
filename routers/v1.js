792d1b80f753dc64d9f7f39f5cb0e6a321f7bf2d
ewaoche <radicemmy@gmail.com>
2022-07-14 15:40:46 +0100

    added auth controller

--- routers/v1.js
+++ routers/v1.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-//conrollers
-const authControllers = require('../controllers/authController');
-
-router.post('/register', authControllers.registerController);
 
 
 router.get('/', (req, res, next) => {
